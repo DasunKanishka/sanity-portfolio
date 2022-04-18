0d21cfe64ef857fb3fb99eedb9dc8fe0e367a984
DK <dasunkanishka92@gmail.com>
2022-04-18 06:00:02 +0530

    Add navbar

--- frontend-react/src/containers/Header/Header.jsx
+++ frontend-react/src/containers/Header/Header.jsx
@@ -1,7 +1,15 @@
+import { Navbar } from '../../components';
+
 import './Header.scss';
 
 const Header = () => {
-	return <h1>Header</h1>;
+	return (
+		<header>
+			<Navbar />
+
+			<h1>Header</h1>
+		</header>
+	);
 };
 
 export default Header;
