3176aed85e94fed027382513634c978059e82700
DK <dasunkanishka92@gmail.com>
2022-04-26 15:41:56 +0530

    Working on testimonials

--- frontend-react/src/containers/Testimonials/Testimonials.jsx
+++ frontend-react/src/containers/Testimonials/Testimonials.jsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+
+import { SectionWrap, MotionWrap } from '../../wrappers';
+import { client, urlFor } from '../../client';
+
 import './Testimonials.scss';
 
 const Testimonials = () => {
-	return <h1>Testimonials</h1>;
+	const [barands, setBarands] = useState([]);
+	const [testimonials, setTestimonials] = useState([]);
+	const [currentTestimonial, setCurrentTestimonial] = useState(0);
+
+	useEffect(() => {
+		const testimonialsQuery = '*[_type == "testimonials"]';
+
+		client
+			.fetch(testimonialsQuery)
+			.then(data => setTestimonials(data))
+			.catch(error => console.error(error));
+	}, []);
+
+	return (
+		testimonials.length > 0 && (
+			<>
+				<div className="testimonials-item app__flex">
+					<img
+						src={urlFor(testimonials[currentTestimonial].imgurl)}
+						alt="Testimonial"
+					/>
+				</div>
+			</>
+		)
+	);
 };
 
-export default Testimonials;
+export default SectionWrap(
+	MotionWrap(Testimonials, 'section-testimonials'),
+	'testimonials',
+	'app__primaryBg'
+);
